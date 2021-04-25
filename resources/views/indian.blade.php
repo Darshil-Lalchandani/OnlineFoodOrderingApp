58b66abc9ef65b2dc9f725cb346511ec5712801d
Darshil Lalchandani <ldarshill@gmail.com>
2021-04-25 22:17:03 +0530

    frontend done

--- resources/views/indian.blade.php
+++ resources/views/indian.blade.php
@@ -4,11 +4,11 @@
 
 <div class="card-deck">
   <div class="card text-white bg-dark">
-    <img class="card-img-top" src="{{URL::asset('/images/pizza.jpg')}}" alt="pizza" style="height:220px;">
+    <img class="card-img-top" src="{{URL::asset('/images/dosa.jpg')}}" alt="dosa">
     <div class="card-body">
-      <h5 class="card-title">Pizza</h5>
-      <p class="card-text">An Exotic dish made of mixed vegetable steamed formed into dumplings deep fried and cooked in a tangy sauce</p>
-      <h6>₹250</h6>
+      <h5 class="card-title">Dhosa</h5>
+      <p class="card-text">Exotic flattened dough with potato stuffings indside. Served with chutney and sambhar.</p>
+      <h6>₹100</h6>
       <a href="#" class="btn btn-primary">Add to cart</a>
     </div>
     <div class="card-footer">
@@ -16,10 +16,10 @@
     </div>
   </div>
   <div class="card text-white bg-dark">
-    <img class="card-img-top" src="{{URL::asset('/images/pasta.jpg')}}" alt="pasta" style="height:260px;">
+    <img class="card-img-top" src="{{URL::asset('/images/punjabi-thali.png')}}" alt="punjabi-thali">
     <div class="card-body">
-      <h5 class="card-title">Pasta</h5>
-      <p class="card-text">Momo is a type of steamed dumpling with fresh vegetable filling</p>
+      <h5 class="card-title">Punjabi Thali</h5>
+      <p class="card-text">1 Paneer Butter Masala, 6 kulcha, Dal, Jeera Rice, Buttermilk.</p>
       <h6>₹280</h6>
       <a href="#" class="btn btn-primary">Add to cart</a>
     </div>
@@ -28,11 +28,11 @@
     </div>
   </div>
   <div class="card text-white bg-dark">
-    <img class="card-img-top" src="{{URL::asset('/images/lasagna.jpg')}}" alt="lasagna" style="height:300px;">
+    <img class="card-img-top" src="{{URL::asset('/images/gujarati-thali.jfif')}}" alt="gujarati-thali">
     <div class="card-body">
-      <h5 class="card-title">Lasagne</h5>
-      <p class="card-text">A narrow strip of unleavened egg dough that has been rolled thin and dried and boiled</p>
-      <h6>₹150</h6>
+      <h5 class="card-title">Gujarati Thali</h5>
+      <p class="card-text">1 Undhyu, 6 butter roti, dal, rice, papad, Buttermilk.</p>
+      <h6>₹250</h6>
       <a href="#" class="btn btn-primary">Add to cart</a>
     </div>
     <div class="card-footer">
@@ -40,11 +40,11 @@
     </div>
   </div>
   <div class="card text-white bg-dark">
-    <img class="card-img-top" src="{{URL::asset('/images/brushetta.jpg')}}" alt="brushetta">
+    <img class="card-img-top" src="{{URL::asset('/images/rasgulla.jpg')}}" alt="rasgulla">
     <div class="card-body">
-      <h5 class="card-title">Brushetta</h5>
+      <h5 class="card-title">Rasgulla</h5>
       <p class="card-text">Fried rice is a dish of cooked rice that has been stir-fried in a frying pan and is mixed with other ingredients such as eggs, vegetables, seafood, or meat.</p>
-      <h6>₹140</h6>
+      <h6>₹40</h6>
       <a href="#" class="btn btn-primary">Add to cart</a>
     </div>
     <div class="card-footer">
